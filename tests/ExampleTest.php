810883fc3c3d32805b5bc97fa047274e8e992002
Desarrollo <Desarrollo@192.168.63.179>
2015-10-12 18:13:35 -0500

    Se crea el dir del API

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -16,4 +16,8 @@ class ExampleTest extends TestCase
         $this->visit('/')
              ->see('Laravel 5');
     }
+    
+    public function testPhpUnit(){
+        $this->assertTrue(true);
+    }
 }
