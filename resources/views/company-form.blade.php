292f3d68e31661d18617912aaca3977226732a4b
alekcis <alekcis27@gmail.com>
2015-10-19 18:41:06 -0500

    vista de formulario de compania

---
+++ resources/views/company-form.blade.php
@@ -0,0 +1,31 @@
+{!! Html::style('assets/css/bootstrap.min.css') !!}
+
+{!! Html::script('assets/js/bootstrap.min.js') !!}
+<title>Jumbotron Template for Bootstrap</title>
+<h3>Administracion Compania</h3>
+
+{!! Form::open(array('url' => 'admin/createCompany')) !!}
+<div class="form-group">
+    {!!    Form::label('name', 'Nombre') !!}
+    {!!    Form::text('name','', array('class' => 'form-control col-3')) !!}
+</div>
+<div class="form-group">    
+    {!!    Form::hidden('plan_id','1', array('class' => 'form-control col-3')) !!}
+</div>
+<div class="form-group">
+    {!!    Form::label('start_date', 'Fecha Inicio') !!}
+    {!!    Form::date('start_date', \Carbon\Carbon::now()) !!}
+</div>
+<div class="form-group">
+    {!!    Form::label('end_date', 'Fecha Fin') !!}
+    {!!    Form::date('end_date', \Carbon\Carbon::now()) !!}
+</div>
+<div class="form-group">
+    {!!    Form::label('last_name', 'Apellido') !!}
+    {!!    Form::text('last_name','', array('class' => 'form-control col-3')) !!}
+</div>
+{!!    Form::submit('realizar post', array('class' => 'btn btn-default')) !!}
+{!! Form::close() !!}
+
+
+
