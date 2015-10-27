6ebb317c8bc7ef95b36a740c76dc8062da7dd149
Alejandro Barrionuevo <alex_neighbournew@hotmail.com>
2015-10-27 12:55:09 -0500

    se modifica el titulo de los roles

--- resources/views/roles-grid.blade.php
+++ resources/views/roles-grid.blade.php
@@ -1,9 +1,7 @@
 @extends('principal')
 @section('contenido')
-
 <title>Roles</title>
-<h3>Administraci&oacute;n Roles</h3>
-
+<h3>Roles</h3>
 <div class="form-group">
     <table class="table table-striped table-bordered table-hover dataTables-example" >
         <thead>
@@ -20,6 +18,4 @@
         </tbody>
     </table>
 </div>
-
-
 @endsection('contenido')
